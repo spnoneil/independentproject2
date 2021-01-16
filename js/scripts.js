76135463a20a067ebae80afdba32ff37c7deec17
Scott O'Neil <scottieoneil@gmail.com>
2021-01-15 16:28:58 -0800

    add jquery effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    window.scrollTo(500, 0);
+    window.scrollTo(1000, 0);
     const q1 = parseInt($("input[name='q1']:checked").val(), 10);
     const q2 = parseInt($("input[name='q2']:checked").val(), 10);
     const q3 = parseInt($("input[name='q3']:checked").val(), 10);
@@ -13,13 +13,13 @@ $(document).ready(function() {
     const result = (q1 + q2 + q3 + q4 + q5 + q6 + q7);
 
     if (result <= 12) {
-      $("#answer1, #form1, #back").toggle();
+      $("#answer1, #form1, #back").fadeToggle(1500);
     } else if (result > 12 && result < 19) {
-      $("#answer2, #form1, #back").toggle();
+      $("#answer2, #form1, #back").fadeToggle(1500);
     } else if (result >= 19 && result < 24) {
-      $("#answer3, #form1, #back").toggle();
+      $("#answer3, #form1, #back").fadeToggle(1500);
     } else if (result >= 24) {
-      $("#answer4, #form1, #back").toggle();
+      $("#answer4, #form1, #back").fadeToggle(1500);
     }
     const userName = $("#userNameInput").val();
     $(".userName").text(userName);
@@ -28,7 +28,7 @@ $(document).ready(function() {
 
   });
   $("#back").click(function() {
-    $("#form1, #back").toggle();
+    $("#form1, #back").slideToggle(1000);
     $("#answer1, #answer2, #answer3, #answer4").hide();
     window.scrollTo(0, 0);
   });
